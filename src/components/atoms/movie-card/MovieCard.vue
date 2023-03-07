f44c08f5d4a38dd9757dc12babc12e2811975d9d
isik-ozan <47965790+isik-ozan@users.noreply.github.com>
2023-03-07 22:51:35 +0100

    feat: add movie card component

---
+++ src/components/atoms/movie-card/MovieCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="card__container">
+    <img class="card--img" :src="image" />
+    <div class="card--rating">{{ rating }}</div>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "MovieCard",
+  components: {},
+  props: {
+    image: {
+      type: String,
+      required: true,
+      default: null,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+@import "./movie-card.scss";
+</style>
