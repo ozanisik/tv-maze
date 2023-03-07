60354aa8fe7ffc73513ff8643bba6d101e2d7a71
isik-ozan <47965790+isik-ozan@users.noreply.github.com>
2023-03-07 23:48:21 +0100

    feat: add navbar molecule component

---
+++ src/components/molecules/navbar/NavBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="navbar">
+    <router-link to="/"
+      ><div class="navbar__logo" to="/">TV Maze</div></router-link
+    >
+    <div class="navbar__action">
+      <search-input></search-input>
+    </div>
+  </section>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import SearchInput from "@/components/atoms/search-input/SearchInput.vue";
+
+export default defineComponent({
+  name: "NavBar",
+  components: { SearchInput },
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+@import "./navbar.scss";
+</style>
