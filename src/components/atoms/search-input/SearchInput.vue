a8104f745a17af0c2647c95b97da647017fd5c52
isik-ozan <47965790+isik-ozan@users.noreply.github.com>
2023-03-07 22:39:12 +0100

    feat: add search input component

---
+++ src/components/atoms/search-input/SearchInput.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="flex justify-center">
+    <div class="mb-3 xl:w-96">
+      <input
+        type="search"
+        v-model="searchValue"
+        class="search-input"
+        data-test-id="search-component"
+        placeholder="Search Show"
+        @input="() => emitter.emit('search', searchValue)"
+      />
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+import useEmitter from "@/composables/useEmitter";
+
+export default defineComponent({
+  name: "SearchInput",
+  components: {},
+  setup() {
+    const searchValue = ref<string>();
+    const emitter = useEmitter();
+
+    return {
+      searchValue,
+      emitter,
+    };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+@import "./search-input.scss";
+</style>
