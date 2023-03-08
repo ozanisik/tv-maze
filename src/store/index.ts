f270035233b9ca60341b045c8fb83515892fb50c
isik-ozan <47965790+isik-ozan@users.noreply.github.com>
2023-03-08 21:57:28 +0100

    chore: some style changes and add serviceWorker

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,18 +1,8 @@
-import { TvShow } from "@/api/tv-shows/TvShow";
-import { getTvShows } from "@/api/tv-shows/TvShows.api";
 import { createStore } from "vuex";
 
 export default createStore({
-  state: {
-    tvShows: [] as Array<TvShow>,
-  },
-  mutations: {
-    FETCH_TV_SHOWS(state) {
-      getTvShows().then((response) => {
-        state.tvShows = response;
-      });
-    },
-  },
+  state: {},
+  mutations: {},
   actions: {},
   modules: {},
 });
